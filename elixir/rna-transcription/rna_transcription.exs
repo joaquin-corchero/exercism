b9249c899d817dfe3ef524dd0c0b7a0667024b0b
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-03-01 09:59:13 +0000

    started execise

---
+++ elixir/rna-transcription/rna_transcription.exs
@@ -0,0 +1,35 @@
+defmodule RNATranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RNATranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @map %{
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }
+
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    result = @map
+    |> Map.fetch(dna)
+    |> get_match(dna)
+  end
+
+  defp get_match({:ok, char}, _), do: char
+  defp get_match(_, _) do
+    IO.inspect "*******************************************"
+    IO.inspect "*******************************************"
+    IO.inspect Map.values(@map)
+    IO.inspect "*******************************************"
+    IO.inspect "*******************************************"
+
+  end
+
+  
+end

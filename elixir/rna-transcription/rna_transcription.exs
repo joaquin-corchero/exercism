90b0ca15bf459b04bdda1d77833641b16ceb414c
JoaquinCorcheroFMP <jcorchero@findmypast.com>
2017-03-21 08:42:09 +0000

    I don't like so many conversions...

--- elixir/rna-transcription/rna_transcription.exs
+++ elixir/rna-transcription/rna_transcription.exs
@@ -8,29 +8,21 @@ defmodule RNATranscription do
   'UGAC'
   """
   @map %{
-    'G' => 'C',
-    'C' => 'G',
-    'T' => 'A',
-    'A' => 'U'
+    "G" => "C",
+    "C" => "G",
+    "T" => "A",
+    "A" => "U"
   }
 
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    |> to_string
+    |> String.split("", trim: true)
+    |> Enum.reduce([], fn(c, acc) -> 
+       acc ++ [to_string @map[c] ]
+    end)
+    |> Enum.join
+    |> to_charlist
+  end
     
-    result = @map
-    |> Map.fetch(dna)
-    |> get_match(dna)
-  end
-
-  defp get_match({:ok, char}, _), do: char
-  defp get_match(_, _) do
-    IO.inspect "*******************************************"
-    IO.inspect "*******************************************"
-    IO.inspect Map.values(@map)
-    IO.inspect "*******************************************"
-    IO.inspect "*******************************************"
-
-  end
-
-  
 end
